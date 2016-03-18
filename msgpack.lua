1fdb32ef03011780e17b37c21e478eda506fea4e
Tim Caswell <tim@creationix.com>
2016-03-17 20:12:12 -0500

    Bump version to 2.2.0 (closes #1)

--- msgpack.lua
+++ msgpack.lua
@@ -1,6 +1,6 @@
 --[[lit-meta
   name = "creationix/msgpack"
-  version = "2.1.0"
+  version = "2.2.0"
   description = "A pure lua implementation of the msgpack format."
   homepage = "https://github.com/creationix/msgpack-lua"
   keywords = {"codec", "msgpack"}
