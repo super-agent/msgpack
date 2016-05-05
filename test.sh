c55a3b21a522a52418fa2e34e635dfcc4cee8891
Tim Caswell <tim@creationix.com>
2016-05-05 16:55:46 -0500

    Make test.sh match travis config exactly

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 set -e
 if [ -z "$LUA" ];
 then
-  LUA=luajit/2.0.4 $0
   LUA=luvit/2.10.1 $0
+  LUA=luajit/2.0.4 $0
   LUA=lua/5.1.5 $0
   LUA=lua/5.2.4 $0
-  LUA=lua/5.3.2 $0
+  # LUA=lua/5.3.2 $0 # There is no bitop library for lua5.3
 else
   echo "Testing on $LUA"
   source .travis/setenv.sh
