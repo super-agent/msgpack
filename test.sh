1fe28e4b713a7757e50119882f4ad2126cbbb821
Tim Caswell <tim@creationix.com>
2016-05-05 16:37:02 -0500

    Add test script for easy local testing on linux box

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+if [ -z "$LUA" ];
+then
+  LUA=luajit/2.0.4 $0
+  LUA=lua/5.1.5 $0
+  LUA=luajit/5.2.4 $0
+  LUA=luajit/5.3.2 $0
+else
+  echo "Testing on $LUA"
+  source .travis/setenv.sh
+  lit install luvit/pretty-print
+  lua test.lua
+fi
