15c94bfaa355697eb3ef7ed02bb7803daf05dee6
Tim Caswell <tim@creationix.com>
2016-05-06 15:11:53 -0500

    Disable plain lua targets in tests, more work is needed to support those properly

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ then
   LUA=luvit-2 $0
   LUA=luajit-2.0 $0
   LUA=luajit-2.1 $0
-  LUA=lua-5.1 $0
-  LUA=lua-5.2 $0
+  # LUA=lua-5.1 $0
+  # LUA=lua-5.2 $0
   # LUA=lua-5.3 $0 # There is no bitop library for lua5.3
 else
   echo "Testing on $LUA"
